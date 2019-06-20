82ba721c05c9d47943a825ca495e6aa182e4912b
Cipriano Freitas <casf1992@gmail.com>
2019-06-20 21:30:14 +0200

    prettierfied

--- src/Weather.js
+++ src/Weather.js
@@ -1,91 +1,89 @@
-import React, { useEffect, useState } from "react";
-import { AreaChart, ResponsiveContainer, Area, XAxis } from "recharts";
+import React, { useEffect, useState } from 'react';
+import { AreaChart, ResponsiveContainer, Area, XAxis } from 'recharts';
 
 const Chart = ({ data }) => (
-  <>
-    <h2>Rainfall</h2>
-    <div style={{ width: "100vw", maxWidth: "900px", height: "100px" }}>
-      <ResponsiveContainer>
-        <AreaChart height={100} data={data}>
-          <XAxis
-            interval={2}
-            dataKey="time"
-            padding={{ left: 20, right: 20 }}
-          />
-          <Area
-            type="monotone"
-            dataKey="value"
-            stroke="#8884d8"
-            strokeWidth={1}
-          />
-        </AreaChart>
-      </ResponsiveContainer>
-    </div>
-  </>
+    <>
+        <h2>Rainfall</h2>
+        <div style={{ width: '100vw', maxWidth: '900px', height: '100px' }}>
+            <ResponsiveContainer>
+                <AreaChart height={100} data={data}>
+                    <XAxis
+                        interval={2}
+                        dataKey="time"
+                        padding={{ left: 20, right: 20 }}
+                    />
+                    <Area
+                        type="monotone"
+                        dataKey="value"
+                        stroke="#8884d8"
+                        strokeWidth={1}
+                    />
+                </AreaChart>
+            </ResponsiveContainer>
+        </div>
+    </>
 );
 
 const Temp = ({ forecast }) => (
-  <>
-    <h3>Temperature</h3>
-    <h1>{forecast.data.temperature}°</h1>
-  </>
+    <>
+        <h3>Temperature</h3>
+        <h1>{forecast.data.temperature}°</h1>
+    </>
 );
 
 const Forecast = ({ forecast }) => (
-  <>
-    {console.log(forecast.data.forecast)}
-    <details>
-      <summary>
-        <b>Forecast</b>
-      </summary>
-      <ul>
-        {forecast.data.forecast.map(f => (
-          <li>
-            <b>
-              {f.maxtemp}° / {f.mintemp}°
-            </b>
-          </li>
-        ))}
-      </ul>
-    </details>
-  </>
+    <>
+        {console.log(forecast.data.forecast)}
+        <details>
+            <summary>
+                <b>Forecast</b>
+            </summary>
+            <ul>
+                {forecast.data.forecast.map(f => (
+                    <li>
+                        <b>
+                            {f.maxtemp}° / {f.mintemp}°
+                        </b>
+                    </li>
+                ))}
+            </ul>
+        </details>
+    </>
 );
 
 const Weather = ({ weather }) => (
-  <>
-    <Temp forecast={weather.forecast} />
-    <Chart data={weather.rainfall} />
-    <Forecast forecast={weather.forecast} />
-  </>
+    <>
+        <Temp forecast={weather.forecast} />
+        <Chart data={weather.rainfall} />
+        <Forecast forecast={weather.forecast} />
+    </>
 );
 
 const Rainfall = () => {
-  function getData() {
-    setWeather(undefined);
-    navigator.geolocation.getCurrentPosition(
-      function(position) {
-        fetch(
-          `https://buienradar-api.herokuapp.com/?latitude=${
-            position.coords.latitude
-          }&longitude=${position.coords.longitude}`
-        )
-          .then(response => response.json())
-          .then(setWeather);
-      },
-      null,
-      { timeout: 10000 }
+    function getData() {
+        setWeather(undefined);
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                fetch(
+                    `https://buienradar-api.herokuapp.com/?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`
+                )
+                    .then(response => response.json())
+                    .then(setWeather);
+            },
+            null,
+            { timeout: 10000 }
+        );
+    }
+    const [weather, setWeather] = useState(undefined);
+    useEffect(() => {
+        getData();
+    }, []);
+    return (
+        <>
+            <button onClick={getData}>Refresh</button>
+            <div>{weather ? <Weather weather={weather} /> : 'Loading...'}</div>
+        </>
     );
-  }
-  const [weather, setWeather] = useState(undefined);
-  useEffect(() => {
-    getData();
-  }, []);
-  return (
-    <>
-      <button onClick={getData}>Refresh</button>
-      <div>{weather ? <Weather weather={weather} /> : "Loading..."}</div>
-    </>
-  );
 };
 
 export default Rainfall;
